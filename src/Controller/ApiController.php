c8434e3dbc438270876d992c604b6c1a3b8f8430
Maks <maxim.tsaruk.1993@gmail.com>
2025-09-19 18:59:57 +0200

    refactor(routes): update application routes

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,22 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Version;
-use App\Repository\AppRepository;
 use App\Repository\EnvRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Psr\Log\LoggerInterface;
 
 #[Route('/api')]
 final class ApiController extends AbstractController
 {
-    #[Route('/env/{uuid}/deployment', name:'deployment_post', methods: ['POST'])]
+    #[Route('/{uuid}/deployment', name:'deployment_post', methods: ['POST'])]
     public function deployment_post(Request $request, string $uuid, LoggerInterface $logger, EnvRepository $envRepository, EntityManagerInterface $em): JsonResponse
-    {   
+    {
         $response = json_decode($request->getContent(), true);
         $logger->info($response['version']);
         $logger->info($uuid);
@@ -33,14 +31,4 @@ final class ApiController extends AbstractController
         return new JsonResponse($response);
     }
 
-    #[Route('/{uuid}', name:'app_get', methods: ['GET'])]
-    public function app_get(Request $request, string $uuid, AppRepository $appRepository, EntityManagerInterface $em, LoggerInterface $logger): JsonResponse
-    {   
-        $app = $appRepository->findOneBy(['uuid'=> $uuid]);
-        $logger->info($app->getName());
-        $envs = $app->getEnvs();
-        return new JsonResponse($envs);
-    }
-
 }
-
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,21 +6,18 @@ use App\Repository\AppRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Request;   
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-
-#[Route('/control')]
+#[Route('/')]
 final class IndexController extends AbstractController
-{   
-    #[Route(path: '/{uuid}/', name: 'get_app_info', methods :['GET'])]
-    public function index_page(string $uuid, LoggerInterface $logger, AppRepository $appRepository) : Response
+{
+    #[Route(path: '{uuid}/', name: 'get_app_info', methods :['GET'])]
+    public function index_page(string $uuid, LoggerInterface $logger, AppRepository $appRepository): Response
     {
         $app = $appRepository->findOneBy(['uuid' => $uuid]);
         $envs = $app->getEnvs();
         $logger->info($envs[0]->getVersions()->last()->getVersion());
-        
-        return $this->render('index/index.html.twig', ['app'=>$app]);
+
+        return $this->render('index/index.html.twig', ['app' => $app]);
     }
 }
